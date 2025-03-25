bb47c6a97efd25be865da9a8d42463816ea86ce7
TienTrong102 <tientrong0210@gmail.com>
2025-03-25 22:33:28 +0700

    delete FAQ

--- src/main/java/com/example/SkinCareSellProductSysterm/Service/FAQService.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/Service/FAQService.java
@@ -1,6 +1,5 @@
 package com.example.SkinCareSellProductSysterm.Service;
 
-import com.example.SkinCareSellProductSysterm.DTO.FAQRequest;
 import com.example.SkinCareSellProductSysterm.Repository.FAQRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
