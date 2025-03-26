aab063110dae2a1004ab58ad96fa6ed15d37319e
TienTrong102 <tientrong0210@gmail.com>
2025-03-26 09:57:57 +0700

    fix OderRequest

--- src/main/java/com/example/SkinCareSellProductSysterm/DTO/OrderRequest.java
+++ src/main/java/com/example/SkinCareSellProductSysterm/DTO/OrderRequest.java
@@ -11,4 +11,23 @@ public class OrderRequest {
     private Long userId;
     private BigDecimal totalPrice;
 
+    // Getter and Setter
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
 }
