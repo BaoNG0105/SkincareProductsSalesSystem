e20b1f779d249ecbf103834c91b8a92f5247f6ed
Viết Hoàng <nguyenviethoanglop10a12@gmail.com>
2025-03-29 18:32:11 +0700

    Forgot to Create AccountUtil Use for Order

---
+++ src/main/java/com/example/SkinCareSellProductSysterm/Utils/AccountUtils.java
@@ -0,0 +1,29 @@
+package com.example.SkinCareSellProductSysterm.Utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountUtils implements ApplicationContextAware {
+
+    private static UserRepository userRepository;
+//
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        userRepository = applicationContext.getBean(UserRepository.class);
+//    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        userRepository = applicationContext.getBean(UserRepository.class);
+    }
+
+    public User getUser(){
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByUserName(userName)
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+}
